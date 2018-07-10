2fd766ced50d28d25d9ed83f46f1ad18b327e798
zjkl19 <eamdfan@126.com>
2018-07-10 11:04:39 +0800

    add features

--- GuanglinzhongBridgeEntity/GuanglinzhongBridgeEntity.py
+++ GuanglinzhongBridgeEntity/GuanglinzhongBridgeEntity.py
@@ -13,7 +13,7 @@ from abaqusConstants import *
 from caeModules import *
 
 from interaction import *
-from optimization import *
+from optimization import * 
 from sketch import *
 from visualization import *
 from connectorBehavior import *
@@ -32,6 +32,9 @@ EC50=3.45E10
 GC50=2.50E10
 niuC50=0.2    #possion ratio
 
+shellThickness=0.1  #the thickness of deck
+shellWidth=18
+
 
 middleBeamOutter=((0.0,0.0),(124,0),(124,25),(117,25),     
         (117,90), (7,90),(7,25),(0,25))
@@ -40,60 +43,106 @@ middleBeamOutter=[(i[0]*0.01,i[1]*0.01) for i in middleBeamOutter]
 middleBeamInner=((28,12),(96,12),(96,78),(28,78))
 middleBeamInner=[(i[0]*0.01,i[1]*0.01) for i in middleBeamInner]
 
-sideBeamOutter=((0.0,0.0),(124,0),(124,73),(124,73),     
-        (124,90), (7,90),(7,25),(0,25))
+sideBeamOutter=((0.0,0.0),(124.5,0),(124.5,73),(124.5+25,73),     
+        (124.5+25,90), (7,90),(7,25),(0,25))
 sideBeamOutter=[(i[0]*0.01,i[1]*0.01) for i in sideBeamOutter]
 
 sideBeamInner=((28,12),(96,12),(96,78),(28,78))
 sideBeamInner=[(i[0]*0.01,i[1]*0.01) for i in sideBeamInner]
 
+shellOutter=((0,0),(18,0),(18,0.1),(0,0.1))
+
 #set the beam section
-def CreateBeamPart(mySketch,Outter,Inner):
+def CreateBeamPart(mySketch,Outter,Inner=()):
     for i in range(0,len(Outter)):
         if(i!=len(Outter)-1):    
             mySketch.Line(point1=Outter[i], point2=Outter[i+1])
         else:
             mySketch.Line(point1=Outter[i], point2=Outter[0])
-
-    for i in range(0,len(Inner)):
-        if(i!=len(Inner)-1):  
-            mySketch.Line(point1=Inner[i], point2=Inner[i+1])
-        else:
-            mySketch.Line(point1=Inner[i], point2=Inner[0])
+    if(len(Inner)!=0):
+        for i in range(0,len(Inner)):
+            if(i!=len(Inner)-1):  
+                mySketch.Line(point1=Inner[i], point2=Inner[i+1])
+            else:
+                mySketch.Line(point1=Inner[i], point2=Inner[0])
     
 
 # Create a model.
 modelName='GuanglinzhongBridge'
 myModel = mdb.Model(name=modelName)
 
+myModel.Material(name='C50Material')
+myModel.materials['C50Material'].Density(table=((rouC50,), ))
+myModel.materials['C50Material'].Elastic(table=((EC50, niuC50), ))
+
+myModel.HomogeneousSolidSection(name='C50Section',material='C50Material', thickness=None)
+
 # Create a sketch
 from part import *
-mySketch = myModel.ConstrainedSketch(name='mySketch',sheetSize=300.0)
+middleBeamSketch = myModel.ConstrainedSketch(name='middleBeamSketch',sheetSize=300.0)
 
 # Create the line.
 
 #middlebeam
-CreateBeamPart(mySketch,middleBeamOutter,middleBeamInner)
-myPart = myModel.Part(name='Part-1', dimensionality=THREE_D, type=DEFORMABLE_BODY)
-myPart.BaseSolidExtrude(sketch=mySketch , depth=span)
-del mdb.models['GuanglinzhongBridge'].sketches['mySketch']
+CreateBeamPart(middleBeamSketch,middleBeamOutter,middleBeamInner)
+myPart = myModel.Part(name='middleBeamPart', dimensionality=THREE_D, type=DEFORMABLE_BODY)
+myPart.BaseSolidExtrude(sketch=middleBeamSketch , depth=span)
 
-mySketch = myModel.ConstrainedSketch(name='mySketch',sheetSize=300.0)
-CreateBeamPart(mySketch,sideBeamOutter,sideBeamInner)
-myPart = myModel.Part(name='Part-2', dimensionality=THREE_D, type=DEFORMABLE_BODY)
-myPart.BaseSolidExtrude(sketch=mySketch , depth=span)
-del mdb.models['GuanglinzhongBridge'].sketches['mySketch']
-#for i in range(0,len(middleBeamOutter)):
-#    if(i!=len(middleBeamOutter)-1):    
-#        mySketch.Line(point1=middleBeamOutter[i], point2=middleBeamOutter[i+1])
-#    else:
-#        mySketch.Line(point1=middleBeamOutter[i], point2=middleBeamOutter[0])
 
-#for i in range(0,len(middleBeamInner)):
-#    if(i!=len(middleBeamInner)-1):  
-#        mySketch.Line(point1=middleBeamInner[i], point2=middleBeamInner[i+1])
-#    else:
-#        mySketch.Line(point1=middleBeamInner[i], point2=middleBeamInner[0])
+sideBeamSketch = myModel.ConstrainedSketch(name='sideBeamSketch',sheetSize=300.0)
+CreateBeamPart(sideBeamSketch,sideBeamOutter,sideBeamInner)
+myPart = myModel.Part(name='sideBeamPart', dimensionality=THREE_D, type=DEFORMABLE_BODY)
+myPart.BaseSolidExtrude(sketch=sideBeamSketch , depth=span)
 
-# Create a three-dimensional, deformable part.
+shellSketch = myModel.ConstrainedSketch(name='shellSketch',sheetSize=300.0)
+CreateBeamPart(shellSketch,shellOutter)
+myPart = myModel.Part(name='shellPart', dimensionality=THREE_D, type=DEFORMABLE_BODY)
+myPart.BaseSolidExtrude(sketch=shellSketch , depth=span)
 
+#section assignment
+c = myModel.parts['middleBeamPart'].cells
+cells = c.findAt(((0.05, 0.05, 13), ))
+region = myModel.parts['middleBeamPart'].Set(cells=cells, name='Set-1')
+myModel.parts['middleBeamPart'].SectionAssignment(region=region, sectionName='C50Section', offset=0.0, 
+    offsetType=MIDDLE_SURFACE, offsetField='', 
+    thicknessAssignment=FROM_SECTION)
+
+c = myModel.parts['sideBeamPart'].cells
+cells = c.findAt(((0.05, 0.05, 13), ))
+region = myModel.parts['sideBeamPart'].Set(cells=cells, name='Set-2')
+myModel.parts['sideBeamPart'].SectionAssignment(region=region, sectionName='C50Section', offset=0.0, 
+    offsetType=MIDDLE_SURFACE, offsetField='', 
+    thicknessAssignment=FROM_SECTION)
+
+c = myModel.parts['shellPart'].cells
+cells = c.findAt(((12, 0.0, 13), ))
+region = myModel.parts['shellPart'].Set(cells=cells, name='Set-3')
+myModel.parts['shellPart'].SectionAssignment(region=region, sectionName='C50Section', offset=0.0, 
+    offsetType=MIDDLE_SURFACE, offsetField='', 
+    thicknessAssignment=FROM_SECTION)
+
+#Assembly
+a = mdb.models['GuanglinzhongBridge'].rootAssembly
+a.DatumCsysByDefault(CARTESIAN)
+p = mdb.models['GuanglinzhongBridge'].parts['middleBeamPart']
+a.Instance(name='middleBeamPart-1', part=p, dependent=ON)
+p = mdb.models['GuanglinzhongBridge'].parts['shellPart']
+a.Instance(name='shellPart-1', part=p, dependent=ON)
+p = mdb.models['GuanglinzhongBridge'].parts['sideBeamPart']
+a.Instance(name='sideBeamPart-1', part=p, dependent=ON)
+
+a = mdb.models['GuanglinzhongBridge'].rootAssembly
+a.translate(instanceList=('shellPart-1', ), vector=(0.0, 0.9, 0.0))
+#: The instance shellPart-1 was translated by 0., 900.E-03, 0. with respect to the assembly coordinate system
+
+a = mdb.models['GuanglinzhongBridge'].rootAssembly
+a.translate(instanceList=('middleBeamPart-1', ), vector=(1.505, 0.0, 0.0))
+#: The instance middleBeamPart-1 was translated by 1.505, 0., 0. with respect to the assembly coordinate system
+a = mdb.models['GuanglinzhongBridge'].rootAssembly
+a.LinearInstancePattern(instanceList=('middleBeamPart-1', ), direction1=(1.0, 
+    0.0, 0.0), direction2=(0.0, 1.0, 0.0), number1=12, number2=1, 
+    spacing1=1.25, spacing2=0.9)
+
+a = mdb.models['GuanglinzhongBridge'].rootAssembly
+a.translate(instanceList=('sideBeamPart-1', ), vector=(16.505, 0.0, 0.0))
+#: The instance sideBeamPart-1 was translated by 16.505, 0., 0. with respect to the assembly coordinate system
